3c9796d4365aaea61436261d26718494b5a27060
Jason Walker-Yung <jasonwalkeryung@gmail.com>
2020-07-31 15:07:02 -0400

    Convert to pathlib

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-#from path import path
+#from pathlib import path
 
 #with (path(__file__).dirname() / 'pyqt_fit' / 'version.txt').open() as f:
     #__version__ = f.read().strip()
@@ -13,14 +13,19 @@ version_filename = os.path.join(os.path.dirname(__file__), 'pyqt_fit', 'version.
 with open(version_filename, "r") as f:
     __version__ = f.read().strip()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 extra = {}
+
 
 setup(name='PyQt-Fit',
       version=__version__,
       description='Parametric and non-parametric regression, with plotting and testing methods.',
       author='Pierre Barbier de Reuille',
       author_email='pierre.barbierdereuille@gmail.com',
-      url=['https://code.google.com/p/pyqt-fit/'],
+      url='https://github.com/PierreBdR/PyQt-fit',
+      download_url='https://github.com/PierreBdR/PyQt-fit/archive/v_1_4_0.tar.gz',
       packages=['pyqt_fit', 'pyqt_fit.functions', 'pyqt_fit.residuals', 'pyqt_fit.test'],
       package_data={'pyqt_fit': ['qt_fit.ui',
                                  'version.txt',
@@ -36,12 +41,14 @@ setup(name='PyQt-Fit',
                         'numpy >=1.5.0',
                         'scipy >=0.10.0',
                         'matplotlib',
-                        'path.py >=2.4.1'
+                        'path >=13.0',
+                        'qtpy >=1.7.1',
                         ],
       extras_require={'Cython': ["Cython >=0.17"]
                       },
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       license='LICENSE.txt',
-      long_description=open('README.txt').read(),
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: X11 Applications :: Qt',
                    'Intended Audience :: Science/Research',
@@ -50,7 +57,7 @@ setup(name='PyQt-Fit',
                    'Operating System :: MacOS :: MacOS X',
                    'Operating System :: Microsoft :: Windows',
                    'Operating System :: POSIX :: Linux',
-                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.6',
                    'Topic :: Scientific/Engineering :: Mathematics',
                    'Topic :: Scientific/Engineering :: Visualization',
                    ],
